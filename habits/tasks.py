44306ef28e15f9ec05155d2c406c611a037228a9
Алексей <abe26@yandex.ru>
2026-04-18 11:17:00 +0300

    202

--- habits/tasks.py
+++ habits/tasks.py
@@ -29,7 +29,6 @@ def send_habit_reminder():
                 logger.info(f"Напоминание отправлено пользователю {habit.user.user.id}")
             except Exception as e:
                 logger.error(
-                    f"Ошибка отправки напоминания для пользователя {habit.user.user.id}: "
-                    f"{e}"
+                    f"Ошибка отправки напоминания для пользователя "
+                    f"{habit.user.user.id}: {e}"
                 )
-
